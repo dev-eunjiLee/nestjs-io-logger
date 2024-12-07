72715b9c2e925ce721000b5cb3e5f6bb0b82fffd
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-08 02:59:48 +0900

    FIX: (logger) @nestjs의 로거 구현하도록 수정

--- libs/nestjs-io-logger/src/nestjs-io-logger.service.ts
+++ libs/nestjs-io-logger/src/nestjs-io-logger.service.ts
@@ -1,11 +1,23 @@
-import { Injectable, Scope } from "@nestjs/common";
+import { Injectable, Scope, LoggerService } from "@nestjs/common";
 
 @Injectable({ scope: Scope.REQUEST })
-export class NestjsIoLoggerService {
+export class NestjsIoLoggerService implements LoggerService {
   private readonly requestId: string;
 
   constructor() {
     this.requestId = new Date().toISOString();
     console.log("😀", this.requestId);
+  }
+
+  log(message: string) {
+    console.log(message);
+  }
+
+  error(message: string) {
+    console.error(message);
+  }
+
+  warn(message: string) {
+    console.warn(message);
   }
 }
