4893dedc22ba9fefbe1fbb6b161c0b3c317d1bbc
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-12-08 13:12:57 +0900

    FIX: (logger - storage) constructor

--- libs/nestjs-io-logger/src/logger-storage.service.ts
+++ libs/nestjs-io-logger/src/logger-storage.service.ts
@@ -3,4 +3,8 @@ import { AsyncLocalStorage } from "async_hooks";
 import { AlsType } from "./type";
 
 @Injectable()
-export class LoggerStorage extends AsyncLocalStorage<AlsType> {}
+export class LoggerStorage extends AsyncLocalStorage<AlsType> {
+  constructor() {
+    super();
+  }
+}
